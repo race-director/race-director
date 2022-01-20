2d23ffb9c5148111b0a03b4367dbb48047dbe882
Axel Padilla <adpadillar25@gmail.com>
2022-01-19 22:15:19 -0700

    Fixed home page when there are no posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ import { post } from "../types";
 import { firebaseApp } from "../utils/firebase";
 
 interface HomePageProps {
-  posts: [post, post, post];
+  posts: post[];
 }
 
 const HomePage: React.FC<HomePageProps> = ({ posts }) => {
@@ -29,70 +29,76 @@ const HomePage: React.FC<HomePageProps> = ({ posts }) => {
       <Navigation></Navigation>
       <div className="max-w-7xl w-full flex flex-col">
         <div className="grid lg:grid-cols-8 lg:grid-rows-2 grid-rows-1 grid-cols-2">
-          <div className="lg:col-span-5 col-span-5 lg:row-span-2 row-span-1">
-            <Link href={`/p/${posts[0].id}`}>
-              <a>
-                <div className="lg:h-[36rem] h-[20rem] md:h-[30rem] relative">
-                  <div className="h-full w-full object-cover overflow-hidden">
-                    <Image
-                      alt={posts[0].coverImage.coverImageCaption}
-                      src={posts[0].coverImage.coverImageUrl}
-                      layout="fill"
-                      className="object-cover"
-                    ></Image>
+          {posts[0] && (
+            <div className="lg:col-span-5 col-span-5 lg:row-span-2 row-span-1">
+              <Link href={`/p/${posts[0].id}`}>
+                <a>
+                  <div className="lg:h-[36rem] h-[20rem] md:h-[30rem] relative">
+                    <div className="h-full w-full object-cover overflow-hidden">
+                      <Image
+                        alt={posts[0].coverImage.coverImageCaption}
+                        src={posts[0].coverImage.coverImageUrl}
+                        layout="fill"
+                        className="object-cover"
+                      ></Image>
+                    </div>
+                    <div className="absolute grid items-end lg:px-6 px-4 py-4 lg:py-12 bottom-0 w-full bg-gradient-to-b from-transparent to-black/80 via-transparent top-0">
+                      <h1 className="lg:text-5xl md:text-4xl text-3xl font-black text-zinc-100">
+                        {posts[0].metadata.headline}
+                      </h1>
+                    </div>
                   </div>
-                  <div className="absolute grid items-end lg:px-6 px-4 py-4 lg:py-12 bottom-0 w-full bg-gradient-to-b from-transparent to-black/80 via-transparent top-0">
-                    <h1 className="lg:text-5xl md:text-4xl text-3xl font-black text-zinc-100">
-                      {posts[0].metadata.headline}
-                    </h1>
-                  </div>
-                </div>
-              </a>
-            </Link>
-          </div>
+                </a>
+              </Link>
+            </div>
+          )}
 
-          <div className="lg:col-span-3 lg:h-72 h-60">
-            <Link href={`/p/${posts[1].id}`}>
-              <a className="">
-                <div className="h-full w-full relative">
-                  <div className="h-full w-full object-cover overflow-hidden">
-                    <Image
-                      alt={posts[1].coverImage.coverImageCaption}
-                      src={posts[1].coverImage.coverImageUrl}
-                      className="object-cover"
-                      layout="fill"
-                    ></Image>
+          {posts[1] && (
+            <div className="lg:col-span-3 lg:h-72 h-60">
+              <Link href={`/p/${posts[1].id}`}>
+                <a className="">
+                  <div className="h-full w-full relative">
+                    <div className="h-full w-full object-cover overflow-hidden">
+                      <Image
+                        alt={posts[1].coverImage.coverImageCaption}
+                        src={posts[1].coverImage.coverImageUrl}
+                        className="object-cover"
+                        layout="fill"
+                      ></Image>
+                    </div>
+                    <div className="absolute grid items-end lg:px-6 px-4 py-4 lg:py-12 bottom-0 w-full bg-gradient-to-b from-transparent to-black/80 via-transparent top-0">
+                      <h1 className="lg:text-2xl md:text-2xl text-xl font-black text-zinc-100">
+                        {posts[1].metadata.headline}
+                      </h1>
+                    </div>
                   </div>
-                  <div className="absolute grid items-end lg:px-6 px-4 py-4 lg:py-12 bottom-0 w-full bg-gradient-to-b from-transparent to-black/80 via-transparent top-0">
-                    <h1 className="lg:text-2xl md:text-2xl text-xl font-black text-zinc-100">
-                      {posts[1].metadata.headline}
-                    </h1>
+                </a>
+              </Link>
+            </div>
+          )}
+          {posts[2] && (
+            <div className="col-span-3 lg:h-72 h-60">
+              <Link href={`/p/${posts[2].id}`}>
+                <a className="">
+                  <div className="h-full w-full relative">
+                    <div className="h-full w-full object-cover overflow-hidden">
+                      <Image
+                        alt={posts[2].coverImage.coverImageCaption}
+                        src={posts[2].coverImage.coverImageUrl}
+                        className="object-cover"
+                        layout="fill"
+                      ></Image>
+                    </div>
+                    <div className="absolute grid items-end lg:px-6 px-4 py-4 lg:py-12 bottom-0 w-full bg-gradient-to-b from-transparent to-black/80 via-transparent top-0">
+                      <h1 className="lg:text-2xl md:text-2xl text-xl font-black text-zinc-100">
+                        {posts[2].metadata.headline}
+                      </h1>
+                    </div>
                   </div>
-                </div>
-              </a>
-            </Link>
-          </div>
-          <div className="col-span-3 lg:h-72 h-60">
-            <Link href={`/p/${posts[2].id}`}>
-              <a className="">
-                <div className="h-full w-full relative">
-                  <div className="h-full w-full object-cover overflow-hidden">
-                    <Image
-                      alt={posts[2].coverImage.coverImageCaption}
-                      src={posts[2].coverImage.coverImageUrl}
-                      className="object-cover"
-                      layout="fill"
-                    ></Image>
-                  </div>
-                  <div className="absolute grid items-end lg:px-6 px-4 py-4 lg:py-12 bottom-0 w-full bg-gradient-to-b from-transparent to-black/80 via-transparent top-0">
-                    <h1 className="lg:text-2xl md:text-2xl text-xl font-black text-zinc-100">
-                      {posts[2].metadata.headline}
-                    </h1>
-                  </div>
-                </div>
-              </a>
-            </Link>
-          </div>
+                </a>
+              </Link>
+            </div>
+          )}
         </div>
       </div>
     </div>
@@ -113,7 +119,7 @@ export const getServerSideProps: GetServerSideProps<
 
   return {
     props: {
-      posts: posts as [post, post, post],
+      posts: posts as post[],
     },
   };
 };
