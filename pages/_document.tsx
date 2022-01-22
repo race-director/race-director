f1f235b451a99f40e5cfd1dbafeb826621377268
Axel Padilla <adpadillar25@gmail.com>
2022-01-21 20:11:01 -0700

    Removed Adsense & added web manifest

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -16,6 +16,7 @@ class MyDocument extends Document {
     return (
       <Html className="scroll-smooth">
         <Head>
+          <link rel="manifest" href="manifest.json" />
           <meta
             name="google-site-verification"
             content="ieFF-dTmh1vXEf5_t7r1t4sQDBzL1qn0VMptrS753rg"
@@ -30,11 +31,6 @@ class MyDocument extends Document {
             href="https://fonts.googleapis.com/css2?family=Open+Sans:ital,wght@0,300;0,400;0,500;0,600;0,700;0,800;1,300;1,400;1,500;1,600;1,700;1,800&display=swap"
             rel="stylesheet"
           />
-          <script
-            async
-            src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4723963638116733"
-            crossOrigin="anonymous"
-          ></script>
         </Head>
         <body>
           <Main />
