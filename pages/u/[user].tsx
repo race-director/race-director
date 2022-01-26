5c3d398880e8fc8e3b8c1860b0dbebeb64b79be8
Axel Padilla <adpadillar25@gmail.com>
2022-01-25 19:44:08 -0700

    Added under construction modal in user page

--- pages/u/[user].tsx
+++ pages/u/[user].tsx
@@ -1,6 +1,9 @@
 import { doc, getDoc, getFirestore } from "firebase/firestore";
+import { AnimatePresence } from "framer-motion";
 import { GetServerSideProps } from "next";
+import { useRouter } from "next/router";
 import React from "react";
+import { Backdrop, Modal } from "../../components/Menus";
 import { user } from "../../types";
 import { firebaseApp } from "../../utils/firebase";
 
@@ -9,9 +12,42 @@ interface UserPageProps {
 }
 
 const UserPage: React.FC<UserPageProps> = ({ userData }) => {
+  const router = useRouter();
+
   return (
     <div>
-      <pre>{JSON.stringify(userData, null, 2)}</pre>
+      <AnimatePresence>
+        {process.env.NODE_ENV !== "development" ? (
+          <Backdrop onClick={() => router.push("/")}>
+            <Modal>
+              <div className="p-8 grid gap-4">
+                <h1 className="text-xl text-zinc-200/90 font-bold uppercase">
+                  Under construction!
+                </h1>
+                <p className="text-zinc-200/70">
+                  This section is still under construction
+                </p>
+                <div className="grid sm:grid-cols-2 grid-cols-1 gap-2 pt-2 text-zinc-200">
+                  <button
+                    onClick={() => router.push("/")}
+                    className="bg-zinc-700 hover:bg-zinc-600 active:scale-90 transform transition-all py-2 uppercase font-bold text- rounded-md"
+                  >
+                    Go back home
+                  </button>
+                  <button
+                    onClick={() => router.push("/")}
+                    className="bg-red-600 text-center hover:bg-red-700 active:scale-90 transform transition-all py-2 uppercase font-bold rounded-md"
+                  >
+                    Cancel
+                  </button>
+                </div>
+              </div>
+            </Modal>
+          </Backdrop>
+        ) : (
+          <pre>{JSON.stringify(userData, null, 2)}</pre>
+        )}
+      </AnimatePresence>
     </div>
   );
 };
