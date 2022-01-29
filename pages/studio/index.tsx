db6f492cc64c624a781479c8f2cbebb966232633
Axel Padilla <adpadillar25@gmail.com>
2022-01-29 16:41:41 -0700

    Studio pagination

--- pages/studio/index.tsx
+++ pages/studio/index.tsx
@@ -1,21 +1,63 @@
-import { collection, getFirestore } from "firebase/firestore";
+import {
+  collection,
+  DocumentData,
+  getFirestore,
+  limit,
+  orderBy,
+  QueryDocumentSnapshot,
+  where,
+} from "firebase/firestore";
 import Head from "next/head";
 import Link from "next/link";
-import React, { useContext } from "react";
-import { useCollectionData } from "react-firebase-hooks/firestore";
-import { editorContent } from "../../components/Editor";
+import React, { useContext, useEffect, useState } from "react";
 import { Navigation } from "../../components/Navigation";
-import { firebaseApp } from "../../utils/firebase";
+import { post } from "../../types";
+import { firebaseApp, paginateQuery } from "../../utils/firebase";
 import { Auth } from "../_app";
 
 interface StudioPageProps {}
 
 const StudioPage: React.FC<StudioPageProps> = () => {
-  const [user] = useContext(Auth);
-  const [snapshot, loading] = useCollectionData(
-    collection(getFirestore(firebaseApp), `users/${user?.uid}/posts`),
-    { idField: "id" }
-  );
+  const [user, userLoading] = useContext(Auth);
+  const [posts, setPosts] = useState<post[]>([]);
+  const [lastDoc, setLastDoc] = useState<QueryDocumentSnapshot<DocumentData>>();
+
+  useEffect(() => {
+    if (user && !userLoading) {
+      loadMore();
+    }
+  }, [userLoading]);
+
+  const loadMore = (lastDoc?: QueryDocumentSnapshot<DocumentData>) => {
+    const db = getFirestore(firebaseApp);
+    const postCollection = collection(db, "posts");
+    const orderPostsBy = orderBy("metadata.createdAt", "desc");
+    const wherePosts = where("metadata.author", "==", user?.uid);
+    const queryLimit = limit(3);
+    if (lastDoc) {
+      paginateQuery(
+        postCollection,
+        [orderPostsBy, queryLimit, wherePosts],
+        lastDoc
+      ).then((docs) => {
+        const lastVisible = docs.docs[docs.docs.length - 1];
+        setLastDoc(lastVisible);
+        let posts = docs.docs.map((doc) => doc.data() as post);
+        setPosts((prevPosts) => [...prevPosts, ...posts]);
+      });
+    } else {
+      paginateQuery(postCollection, [
+        orderPostsBy,
+        queryLimit,
+        wherePosts,
+      ]).then((docs) => {
+        const lastVisible = docs.docs[docs.docs.length - 1];
+        setLastDoc(lastVisible);
+        let posts = docs.docs.map((doc) => doc.data() as post);
+        setPosts((prevPosts) => [...prevPosts, ...posts]);
+      });
+    }
+  };
 
   return (
     <div className="dark:bg-zinc-900 min-h-screen w-full flex items-center flex-col relative">
@@ -29,25 +71,24 @@ const StudioPage: React.FC<StudioPageProps> = () => {
             <div className="grid gap-6 px-4 py-8 md:py-12">
               <h1 className="text-4xl text-zinc-200 font-bold">Your posts</h1>
               <div className="grid gap-4">
-                {!loading && snapshot?.length ? (
-                  snapshot.map((p) => {
-                    const { coverImage, headline, summary } =
-                      p as unknown as editorContent;
+                {posts?.length ? (
+                  posts.map((p) => {
+                    const { coverImage, id, metadata } = p;
                     return (
-                      <Link key={p.id} href={`p/${p.id}`}>
+                      <Link key={id} href={`p/${id}`}>
                         <a>
                           <div className="flex flex-col lg:flex-row space-x-4 border border-zinc-200 rounded-md overflow-hidden">
-                            <div>
+                            <div className="lg:h-44 aspect-video bg-black">
                               <img
-                                className="lg:h-44 aspect-video object-cover w-full"
+                                className="object-cover w-full"
                                 src={coverImage.coverImageUrl}
                               ></img>
                             </div>
                             <div className="text-zinc-200 py-4">
                               <h1 className="text-2xl font-bold tracking-tighter">
-                                {headline}
+                                {metadata.headline}
                               </h1>
-                              <p>{summary}</p>
+                              <p>{metadata.summary}</p>
                             </div>
                           </div>
                         </a>
@@ -67,6 +108,12 @@ const StudioPage: React.FC<StudioPageProps> = () => {
                     </div>
                   </div>
                 )}
+                <button
+                  onClick={() => loadMore(lastDoc)}
+                  className="text-lg text-zinc-200/80 py-2 px-4 border-zinc-200/70 border-2 rounded-md font-semibold transform active:scale-95 transition-all"
+                >
+                  Load more
+                </button>
               </div>
             </div>
           </div>
